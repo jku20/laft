c1550d721fa0398995cff521e5f8d367fb12f33b
jeremy ku-benjet <jeremykubenjetschool@gmail.com>
2025-11-21 20:28:29 -0500

    Minimal building code.

--- mcu/src/main.c
+++ mcu/src/main.c
@@ -1,5 +1,16 @@
+#include "bsp/board_api.h"
+#include "pico/stdlib.h"
+#include "tusb.h"
+
 #include "tusb_config.h"
 #include "usb_descriptors.h"
 
 int main() {
+  stdio_init_all();
+  board_init();
+  tusb_init();
+
+  while (true) {
+    tud_task();
+  }
 }
