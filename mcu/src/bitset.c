6446bc91128f18ddaeec6149abad0dc48b83fbfa
jeremy ku-benjet <jeremykubenjetschool@gmail.com>
2025-12-07 15:32:54 -0500

    Implement some more of that protocol.

---
+++ mcu/src/bitset.c
@@ -0,0 +1,16 @@
+#include "bitset.h"
+#include <stdbool.h>
+
+Bitset32 bitset_from_uint32(uint32_t value) {
+  Bitset32 out = {.data = value};
+  return out;
+}
+void bitset_set(Bitset32 *self, int idx, bool value) {
+  self->data |= value << idx;
+}
+
+bool bitset_get(Bitset32 *self, int idx) {
+  return (self->data & (1 << idx)) >> idx;
+}
+
+uint32_t bitset_get_raw(Bitset32 *self) { return self->data; }
--- mcu/src/logic_analyzer.c
+++ mcu/src/logic_analyzer.c
@@ -4,9 +4,32 @@
 #include <stdio.h>
 
 void la_set_paused(LogicAnalyzer *self, bool paused) { self->paused = paused; }
-void la_init(LogicAnalyzer *self) { self->paused = false; }
+
+void la_reset(LogicAnalyzer *self) {
+  for (int i = 0; i < NUM_STAGES; i++) {
+    self->trigger_mask[i] = bitset_from_uint32(0);
+    self->trigger_value[i] = bitset_from_uint32(0);
+  }
+  self->paused = false;
+}
+
 uint8_t *la_get_id(LogicAnalyzer *self) { return (uint8_t *)LOGIC_ANALYZR_ID; }
+
 void la_arm(LogicAnalyzer *self) {}
+
+void la_set_trigger_mask(LogicAnalyzer *self, Bitset32 mask, int stage) {
+  self->trigger_mask[stage] = mask;
+}
+
+void la_set_trigger_value(LogicAnalyzer *self, Bitset32 value, int stage) {
+  self->trigger_value[stage] = value;
+}
+
+void la_set_trigger_config(LogicAnalyzer *self, TriggerConfiguration config,
+                           int stage) {
+  self->trigger_config[stage] = config;
+}
+
 void la_transmit(LogicAnalyzer *_self, uint8_t *buf, int len) {
   for (int i = 0; i < len; i++) {
     putchar(buf[i]);
@@ -14,7 +37,8 @@ void la_transmit(LogicAnalyzer *_self, uint8_t *buf, int len) {
 }
 
 void la_queue_command(LogicAnalyzer *self, SumpCommand *cmd) {
-  switch (sump_get_type(cmd)) {
+  switch (sc_get_ty(cmd)) {
+    int stage;
   case Reset:
     la_set_paused(self, false);
     break;
@@ -31,7 +55,49 @@ void la_queue_command(LogicAnalyzer *self, SumpCommand *cmd) {
   case Xoff:
     la_set_paused(self, true);
     break;
+  case SetTriggerMaskStage0:
+  case SetTriggerMaskStage1:
+  case SetTriggerMaskStage2:
+  case SetTriggerMaskStage3:
+    stage = sc_get_stage(cmd);
+    Bitset32 mask = sc_get_mask(cmd);
+    la_set_trigger_mask(self, mask, stage);
+    break;
+  case SetTriggerValueStage0:
+  case SetTriggerValueStage1:
+  case SetTriggerValueStage2:
+  case SetTriggerValueStage3:
+    stage = sc_get_stage(cmd);
+    Bitset32 value = sc_get_value(cmd);
+    la_set_trigger_value(self, value, stage);
+    break;
+  case SetTriggerConfigStage0:
+  case SetTriggerConfigStage1:
+  case SetTriggerConfigStage2:
+  case SetTriggerConfigStage3:
+    stage = sc_get_stage(cmd);
+    TriggerConfiguration config = sc_get_trigger_configuration(cmd);
+    la_set_trigger_config(self, config, stage);
+    break;
   }
 }
 
-SumpCommandType sump_get_type(SumpCommand *self) { return self->ty; }
+SumpCommandType sc_get_ty(SumpCommand *self) { return self->ty; }
+
+Bitset32 sc_get_mask(SumpCommand *self) {
+  return bitset_from_uint32(self->data);
+}
+
+Bitset32 sc_get_value(SumpCommand *self) {
+  return bitset_from_uint32(self->data);
+}
+
+int sc_get_stage(SumpCommand *self) {
+  SumpCommandType ty = sc_get_ty(self);
+  return (ty & 0b00001100) >> 2;
+}
+
+TriggerConfiguration sc_get_trigger_configuration(SumpCommand *self) {
+  TriggerConfiguration out;
+  out.delay = (0xFFFF0000 & self->data) >> 16;
+}
