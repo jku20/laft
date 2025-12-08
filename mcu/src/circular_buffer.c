9cf4b7cf6249db002d25d56d1bccbf268a478211
jeremy ku-benjet <jeremykubenjetschool@gmail.com>
2025-12-08 02:39:00 -0500

    Template the new pio compiler.

--- mcu/src/circular_buffer.c
+++ mcu/src/circular_buffer.c
@@ -3,25 +3,14 @@
 #include <stdbool.h>
 
 #include "circular_buffer.h"
+#include "logic_analyzer.h"
 
 void cb_init(CircularBuffer *self, uint base_pin, PIO pio, uint sm, uint dma) {
   self->pio = pio;
   self->sm = sm;
   self->dma = dma;
 
-  // Create a pio program with a single "in" instruction.
-  uint16_t in_insn = pio_encode_in(pio_pins, base_pin);
-  struct pio_program capture_prog = {
-      .instructions = &in_insn, .length = 1, .origin = -1};
-
-  // Adds the pio program to the state machine and sets the base pin. Also sets
-  // the shift parameters to be compatible with what is expected by the DMA
-  // channel.
-  int offset = pio_add_program(pio, &capture_prog);
-  pio_sm_config pio_config = pio_get_default_sm_config();
-  sm_config_set_wrap(&pio_config, offset, offset);
-  sm_config_set_in_pin_base(&pio_config, base_pin);
-  sm_config_set_in_shift(&pio_config, true, true, 16);
+  // Disable the PIO so the DMA channel doesn't read nonsense.
   pio_sm_set_enabled(pio, sm, false);
 
   // Sets the DMA channel to forever copy from the PIO to the circular buffer.
@@ -33,6 +22,11 @@ void cb_init(CircularBuffer *self, uint base_pin, PIO pio, uint sm, uint dma) {
       dma, &dma_data_config, self->buf, pio->rxf,
       dma_encode_transfer_count_with_self_trigger(BUFFER_SIZE), true);
 }
+
+void cb_arm_to_start_collecting(Bitset32 trigger_mask[NUM_STAGES],
+                                Bitset32 trigger_value[NUM_STAGES],
+                                Bitset32 trigger_config[NUM_STAGES],
+                                uint32_t clock_div) {}
 
 void cb_stop_buf_population(CircularBuffer *self) {
   pio_sm_set_enabled(self->pio, self->sm, false);
