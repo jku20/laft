09660a271a7e729f8c7c5753fc8d3e01956b3ca3
jeremy ku-benjet <jeremykubenjetschool@gmail.com>
2025-12-08 16:17:20 -0500

    Implement the la thing.

--- mcu/include/logic_analyzer.h
+++ mcu/include/logic_analyzer.h
@@ -1,10 +1,10 @@
 #pragma once
 
+#include <hardware/pio.h>
 #include <stdbool.h>
 #include <stdint.h>
 
 #include "bitset.h"
-#include "circular_buffer.h"
 
 #define NUM_STAGES 4
 
@@ -28,6 +28,51 @@ typedef struct {
   bool start;
 } TriggerConfiguration;
 
+// This buffer must be large enough to hold all of the data collected by a SUMP
+// request. That is 2^16 bits. We double this because our timing is not quite
+// cycle accurate for stopping the DMA channel and we want the extra buffer room
+// so we don't overwrite data we need when we jump back to the front of the
+// buffer.
+#define BUFFER_SIZE ((1 << 16) * 2)
+
+/**
+ * This is a circular buffer and associated functions to set up a startable and
+ * stopable DMA channel and PIO state machine which will read 16 consecutive
+ * bits, starting at a base GPIO, into the buffer.
+ */
+typedef struct {
+  uint16_t buf[BUFFER_SIZE];
+  PIO pio;
+  uint sm;
+  uint dma;
+  uint base_pin;
+} CircularBuffer;
+
+/**
+ * Initializes the CircularBuffer and starts the DMA channel and GPIO. This
+ * requires a DMA channel and a PIO state machine.
+ */
+void cb_init(CircularBuffer *self, uint base_pin, PIO pio, uint sm, uint dma);
+
+/** Sets up the PIO state machine to start collecting data based on a trigger.
+ * We do not implement triggering on specific levels as due to limitations from
+ * PIO blocks, we can't well run multiple stages at the same time. Instead, we
+ * run stages with nonzero trigger masks ordered 0 to NUM_STAGES. Additionally,
+ * particularly bad masks (alternating 1s and 0s) may cause the PIO memory to
+ * overflow. We just assume this doesn't happen and silently do undefined
+ * horrors.*/
+void cb_arm_to_start_collecting(CircularBuffer *self,
+                                Bitset32 trigger_mask[NUM_STAGES],
+                                Bitset32 trigger_value[NUM_STAGES],
+                                TriggerConfiguration trigger_config[NUM_STAGES],
+                                uint32_t clock_div, int transfer_count);
+
+/** Stops reading to the circular buffer from the PIO. */
+void cb_stop_buf_population(CircularBuffer *self);
+
+/** Starts reading to the circular buffer from the PIO. */
+void cb_start_buf_population(CircularBuffer *self);
+
 /**
  * Of these flags, we only have support for channel groups and at that only
  * channel groups 0 and 1.
@@ -48,8 +93,6 @@ typedef struct {
   bool inverted;
 } LogicAnalyzerFlags;
 
-const uint8_t LOGIC_ANALYZR_ID[] = {'1', 'A', 'L', 'S'};
-const uint8_t SUMP_ID_LEN = 4;
 typedef struct {
   CircularBuffer buf;
 
@@ -114,7 +157,6 @@ void la_set_clock_divider(LogicAnalyzer *self, uint32_t clock_div);
 void la_set_read_count(LogicAnalyzer *self, uint16_t read_count);
 void la_set_delay_count(LogicAnalyzer *self, uint16_t delay_count);
 void la_set_flags(LogicAnalyzer *self, LogicAnalyzerFlags flags);
-void la_transmit(LogicAnalyzer *self, uint8_t *buf, int len);
 void la_exec_command(LogicAnalyzer *self, SumpCommand *cmd);
 
 SumpCommandType sc_get_ty(SumpCommand *self);
--- mcu/src/logic_analyzer.c
+++ mcu/src/logic_analyzer.c
@@ -1,12 +1,35 @@
-#include "logic_analyzer.h"
+#include <hardware/dma.h>
+#include <hardware/pio.h>
+#include <hardware/pio_instructions.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+
+#include "logic_analyzer.h"
+
+static int read_count = 0;
+static LogicAnalyzer *la;
+
+const uint8_t LOGIC_ANALYZR_ID[] = {'1', 'A', 'L', 'S'};
+const uint8_t SUMP_ID_LEN = 4;
+
+void dma_irq_0_handler() {
+  for (int i = 0; i < read_count; i++) {
+    putchar(0x00FF & la->buf.buf[i]);
+    putchar(0xFF00 & la->buf.buf[i]);
+    putchar(0x00FF & la->buf.buf[i]);
+    putchar(0xFF00 & la->buf.buf[i]);
+  }
+}
 
 void la_set_paused(LogicAnalyzer *self, bool paused) { self->paused = paused; }
 
 void la_init(LogicAnalyzer *self, uint base_pin, PIO pio, uint sm, uint dma) {
   cb_init(&self->buf, base_pin, pio, sm, dma);
+  la = self;
+  irq_set_exclusive_handler(DMA_IRQ_0, dma_irq_0_handler);
+  irq_set_enabled(DMA_IRQ_0, true);
   for (int i = 0; i < NUM_STAGES; i++) {
     self->trigger_mask[i] = bitset_from_uint32(0);
     self->trigger_value[i] = bitset_from_uint32(0);
@@ -30,7 +53,12 @@ void la_reset(LogicAnalyzer *self) {
 
 uint8_t *la_get_id(LogicAnalyzer *self) { return (uint8_t *)LOGIC_ANALYZR_ID; }
 
-void la_arm(LogicAnalyzer *self) {}
+void la_arm(LogicAnalyzer *self) {
+  cb_arm_to_start_collecting(&self->buf, self->trigger_mask,
+                             self->trigger_value, self->trigger_config,
+                             self->clock_div, self->read_count >> 1);
+  read_count = self->read_count >> 1;
+}
 
 void la_set_trigger_mask(LogicAnalyzer *self, Bitset32 mask, int stage) {
   self->trigger_mask[stage] = mask;
@@ -61,7 +89,7 @@ void la_set_flags(LogicAnalyzer *self, LogicAnalyzerFlags flags) {
   self->flags = flags;
 }
 
-void la_transmit(LogicAnalyzer *_self, uint8_t *buf, int len) {
+void transmit_buf(uint8_t *buf, int len) {
   for (int i = 0; i < len; i++) {
     putchar(buf[i]);
   }
@@ -78,7 +106,7 @@ void la_exec_command(LogicAnalyzer *self, SumpCommand *cmd) {
     break;
   case Id:;
     uint8_t *buf = la_get_id(self);
-    la_transmit(self, buf, SUMP_ID_LEN);
+    transmit_buf(buf, SUMP_ID_LEN);
     break;
   case Xon:
     la_set_paused(self, false);
@@ -205,4 +233,119 @@ SumpCommand sc_read_from_stdin() {
     out.data |= getchar();
   }
   return out;
+}
+
+void cb_init(CircularBuffer *self, uint base_pin, PIO pio, uint sm, uint dma) {
+  self->pio = pio;
+  self->sm = sm;
+  self->dma = dma;
+  self->base_pin = base_pin;
+
+  // Disable the PIO so the DMA channel doesn't read nonsense.
+  pio_sm_set_enabled(pio, sm, false);
+
+  // Sets the DMA channel to forever copy from the PIO to the circular buffer.
+  dma_channel_config dma_data_config = dma_channel_get_default_config(dma);
+  channel_config_set_read_increment(&dma_data_config, false);
+  channel_config_set_write_increment(&dma_data_config, true);
+  channel_config_set_dreq(&dma_data_config, pio_get_dreq(pio, sm, false));
+  dma_channel_configure(
+      dma, &dma_data_config, self->buf, pio->rxf,
+      dma_encode_transfer_count_with_self_trigger(BUFFER_SIZE), true);
+}
+
+int trailing_zeros(uint32_t n) {
+  int c = 0;
+  while (!(n & 1) && c < 32) {
+    c++;
+    n >>= 1;
+  }
+  return c;
+}
+
+int trailing_ones(uint32_t n) {
+  int c = 0;
+  while (n & 1) {
+    c++;
+    n >>= 1;
+  }
+  return c;
+}
+
+void cb_arm_to_start_collecting(CircularBuffer *self,
+                                Bitset32 trigger_mask[NUM_STAGES],
+                                Bitset32 trigger_value[NUM_STAGES],
+                                TriggerConfiguration trigger_config[NUM_STAGES],
+                                uint32_t clock_div, int transfer_count) {
+  // This implementation is heavily inspired by the implementation of
+  // [this](https://github.com/dotcypress/ula/blob/40756c4199c9f2ac605a39bfed2eddcc7fe0324b/src/trigger.rs#L65)
+  // function in ula, a logic analyzer implemented for the pico.
+  //
+  // Generate the program for the PIO.
+  uint16_t prog[32];
+  memset(prog, 0, sizeof prog);
+  int static_pc = 0;
+  for (int i = 0; i < NUM_STAGES; i++) {
+    int stage_offset = static_pc;
+    if (bitset_is_empty(&trigger_mask[i])) {
+      continue;
+    }
+
+    uint32_t mask = bitset_get_raw(&trigger_mask[i]);
+    uint32_t value = bitset_get_raw(&trigger_value[i]);
+
+    prog[static_pc++] = pio_encode_mov(pio_osr, pio_pins);
+    while (mask) {
+      int mask_zeros = trailing_zeros(mask);
+      if (mask_zeros) {
+        prog[static_pc++] = pio_encode_out(pio_null, mask_zeros);
+        mask >>= mask_zeros;
+        value >>= mask_zeros;
+      }
+
+      int mask_ones = MIN(trailing_ones(mask), 5);
+      if (mask_ones) {
+        prog[static_pc++] = pio_encode_out(pio_x, mask_ones);
+        prog[static_pc++] =
+            pio_encode_set(pio_y, value & ((1 << mask_ones) - 1));
+        prog[static_pc++] = pio_encode_jmp_x_ne_y(stage_offset);
+        mask >>= mask_ones;
+        value >>= mask_ones;
+      }
+    }
+  }
+  prog[static_pc++] = pio_encode_in(pio_pins, 16);
+
+  // Assign the PIO program to the state machine
+  struct pio_program capture_prog = {
+      .instructions = prog, .length = static_pc, .origin = -1};
+  int offset = pio_add_program(self->pio, &capture_prog);
+  pio_sm_config capture_config = pio_get_default_sm_config();
+  sm_config_set_wrap(&capture_config, offset + static_pc - 1,
+                     offset + static_pc - 1);
+  sm_config_set_in_pin_base(&capture_config, self->base_pin);
+  sm_config_set_in_pin_count(&capture_config, 16);
+  sm_config_set_in_shift(&capture_config, true, true, 16);
+  sm_config_set_clkdiv(&capture_config, clock_div);
+  pio_sm_init(self->pio, self->sm, offset, &capture_config);
+  pio_sm_set_enabled(self->pio, self->sm, true);
+
+  // Sets the DMA channel to copy from the PIO to the circular buffer.
+  dma_channel_config dma_data_config =
+      dma_channel_get_default_config(self->dma);
+  channel_config_set_read_increment(&dma_data_config, false);
+  channel_config_set_write_increment(&dma_data_config, true);
+  channel_config_set_dreq(&dma_data_config,
+                          pio_get_dreq(self->pio, self->sm, false));
+  dma_channel_configure(self->dma, &dma_data_config, self->buf, self->pio->rxf,
+                        dma_encode_transfer_count(transfer_count), true);
+  dma_set_irq0_channel_mask_enabled(1 << self->dma, true);
+}
+
+void cb_stop_buf_population(CircularBuffer *self) {
+  pio_sm_set_enabled(self->pio, self->sm, false);
+}
+
+void cb_start_buf_population(CircularBuffer *self) {
+  pio_sm_set_enabled(self->pio, self->sm, true);
 }
